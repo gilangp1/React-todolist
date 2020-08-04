eca54a3c79678bfe9f88fd5a8e45ed9aa9f8d9ce
gilangp1 <68611276+gilangp1@users.noreply.github.com>
2020-08-05 02:40:56 +0700

    menambahkan css module pada todos

---
+++ src/components/todos/Todos.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styles from "./todos.module.css";
+
+import Todo from "../todo/Todo";
+
+const Todos = ({ todos, completeTodo }) => {
+  return (
+    <section className={styles.todos}>
+      {todos.length > 0 &&
+        todos.map((todo, index) => {
+          return (
+            <Todo
+              key={index}
+              text={todo.text}
+              isCompleted={todo.isCompleted}
+              completeTodo={completeTodo}
+              index={index}
+            />
+          );
+        })}
+      {todos.length === 0 && (
+        <div className={styles.todoPlaceholderText}>
+          add todo by clicking{" "}
+          <span className={styles.addButtonPlaceholderText}>ADD</span> button on
+          the top of left corner
+        </div>
+      )}
+    </section>
+  );
+};
+
+Todos.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string
+    })
+  ),
+  completeTodo: PropTypes.func.isRequired
+};
+
+export default Todos;
